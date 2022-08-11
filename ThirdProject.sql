53dab523af68bf2fb399c20ad0b5afb3d319210f
Gabriela <gabi16.hernandez@gmail.com>
2022-08-11 09:34:27 -0600

    Mas consultas

--- ThirdProject.sql
+++ ThirdProject.sql
@@ -1,20 +1,17 @@
 -- Listado de jugadores que al menos han jugado una partida
 SELECT DISTINCT player.id_player, player.playername FROM player
-INNER JOIN playerturn
-ON player.id_player = playerturn.id_playerturn;
+INNER JOIN playerturn ON player.id_player = playerturn.id_playerturn;
 
 -- Listado de jugadores que no han jugado una partida
 SELECT  player.id_player, player.playername FROM player
-LEFT JOIN playerturn
-ON player.id_player = playerturn.id_playerturn
+LEFT JOIN playerturn ON player.id_player = playerturn.id_playerturn
 WHERE playerturn.id_playerturn IS NULL;
 
 -- Listado de jugadores que al menos han ganado una partida
 SELECT DISTINCT player.id_player, player.playername FROM player
-INNER JOIN playerturn
-ON player.id_player = playerturn.id_playerturn
+INNER JOIN playerturn ON player.id_player = playerturn.id_playerturn
 WHERE playerturn.winn = 1;
-
+---IDK
 SELECT id_player, playername FROM player
 WHERE win_points > 0;
 
@@ -33,25 +30,23 @@ ORDER BY win_points DESC
 FETCH NEXT 3 ROWS ONLY;
 
 -- Top 3 de los mejores jugadores hangman
-SELECT COUNT(playerturn.id_playerturn) 
-AS winn_hm, player.playername FROM player
+SELECT COUNT(playerturn.id_playerturn) AS winn_hm, player.playername FROM player
 INNER JOIN playerturn
 ON player.id_player = playerturn.id_player
 INNER JOIN turn
 ON turn.id_turn = playerturn.turn_id_turn
-WHERE id_game = 002 AND playerturn.winn = 1
+WHERE game_id_game = 002 AND playerturn.winn = 1
 GROUP BY playerturn.id_player, player.playername
 ORDER BY winn_hm DESC
 FETCH NEXT 3 ROWS ONLY;
 
 -- Top 3 de los mejores jugadores ttt
-SELECT COUNT(playerturn.id_playerturn) 
-AS winn_ttt, player.playername FROM player
+SELECT COUNT(playerturn.id_playerturn) AS winn_ttt, player.playername FROM player
 INNER JOIN playerturn
 ON player.id_player = playerturn.id_player
 INNER JOIN turn
 ON turn.id_turn = playerturn.turn_id_turn
-WHERE id_game = 001 AND playerturn.winn = 1
+WHERE game_id_game = 001 AND playerturn.winn = 1
 GROUP BY playerturn.id_player, player.playername
 ORDER BY winn_ttt DESC
 FETCH NEXT 3 ROWS ONLY;
@@ -63,7 +58,7 @@ INNER JOIN playerturn
 ON player.id_player = playerturn.id_player
 INNER JOIN turn
 ON turn.id_turn = playerturn.turn_id_turn
-WHERE id_game = 002 AND playerturn.winn = 0
+WHERE game_id_game = 002 AND playerturn.winn = 0
 GROUP BY playerturn.id_player, player.playername
 ORDER BY loose_hm DESC
 FETCH NEXT 3 ROWS ONLY;
@@ -75,9 +70,63 @@ INNER JOIN playerturn
 ON player.id_player = playerturn.id_player
 INNER JOIN turn
 ON turn.id_turn = playerturn.turn_id_turn
-WHERE id_game = 001 AND playerturn.winn = 0
+WHERE game_id_game = 001 AND playerturn.winn = 0
 GROUP BY playerturn.id_player, player.playername
 ORDER BY loose_ttt DESC
 FETCH NEXT 3 ROWS ONLY;
 
+-- Jugador de TTT con menos tiros para ganar
+SELECT MIN(playerturn.opportunities) AS OP, player.playername
+FROM player
+INNER JOIN playerturn
+ON player.id_player = playerturn.id_player
+INNER JOIN turn
+ON turn.id_turn = playerturn.turn_id_turn
+WHERE game_id_game = 001 
+GROUP BY player.id_player, player.playername
+ORDER BY OP ASC
+FETCH NEXT 1 ROWS ONLY;
 
+-- Jugador de Hangman con menos intentos al ganar
+SELECT MIN(playerturn.opportunities) AS OP, player.playername FROM player
+INNER JOIN playerturn
+ON player.id_player = playerturn.id_player
+INNER JOIN turn
+ON turn.id_turn = playerturn.turn_id_turn
+WHERE game_id_game = 002 
+GROUP BY player.id_player, player.playername
+ORDER BY OP ASC
+FETCH NEXT 1 ROWS ONLY;
+
+-- Jugador de TTT con más tiros para ganar
+SELECT MAX(playerturn.opportunities) AS OP, player.playername FROM player
+INNER JOIN playerturn
+ON player.id_player = playerturn.id_player
+INNER JOIN turn
+ON turn.id_turn = playerturn.turn_id_turn
+WHERE game_id_game = 001 
+GROUP BY player.id_player, player.playername
+ORDER BY OP DESC
+FETCH NEXT 1 ROWS ONLY;
+
+-- Jugador de Hangman con más intentos para ganarend
+SELECT MAX(playerturn.opportunities) AS OP, player.playername FROM player
+INNER JOIN playerturn
+ON player.id_player = playerturn.id_player
+INNER JOIN turn
+ON turn.id_turn = playerturn.turn_id_turn 
+WHERE game_id_game = 002 
+GROUP BY player.id_player, player.playername
+ORDER BY OP DESC
+FETCH NEXT 1 ROWS ONLY;
+
+-- Promedio de tiros en TTT para ganar una partida ( partidas empatadas y en progreso no cuentan)
+SELECT AVG(playerturn.opportunities) AS "Average Of Opportunities" FROM turn
+INNER JOIN playerturn
+ON turn.id_turn = playerturn.turn_id_turn
+WHERE game_id_game = 001 AND end = 1;
+
+-- Promedio del tamaño de palabras utilizadas en Hangman
+SELECT AVG(COUNT(*)) AS "Average of words"
+FROM secretword
+GROUP BY id_hangedman;
